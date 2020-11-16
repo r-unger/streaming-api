5f51914845070cbe846494a5ab2855849e3874b3
fred42vid <fred42vid@gmail.com>
2020-11-16 12:17:44 +0100

    no need to have special code for embedded tomcat vs standalone tomcat; it's the same for both

--- src/main/java/com/videotools/streamingapi/MainAppStreamingApi.java
+++ src/main/java/com/videotools/streamingapi/MainAppStreamingApi.java
@@ -7,10 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MainAppStreamingApi {
 
     public static void main(String[] args) {
-// Run locally with the embedded tomcat:
-//        SpringApplication.run(MainAppStreamingApi.class, args);
-// Build deployable war file:
-        SpringApplication.run(GreetingController.class, args);
+        SpringApplication.run(MainAppStreamingApi.class, args);
     }
 }
 
