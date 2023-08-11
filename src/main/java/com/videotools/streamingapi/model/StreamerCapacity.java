b13a982ed8defba2e7c13bdeace6711c40121668
fred42vid <fred42vid@gmail.com>
2023-08-11 11:23:33 +0200

    added workload logic

--- src/main/java/com/videotools/streamingapi/model/StreamerCapacity.java
+++ src/main/java/com/videotools/streamingapi/model/StreamerCapacity.java
@@ -5,6 +5,8 @@
  */
 package com.videotools.streamingapi.model;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  *
  * @author xyz
@@ -12,41 +14,93 @@ package com.videotools.streamingapi.model;
 public class StreamerCapacity {
     
     private final String hostname;
-    private final int absoluteCapacity;
-    private final int nominalCapacity;
-    private final int priority;
+    private volatile int hundredPercentCapacity;
+    private volatile int absoluteCapacity;
+    private volatile int priority;
         // priority can only be 0, 1, 2, 3
         // perhaps an ENUM would be better, but how should it be stored?
         // 3: fill firs
         // 2: fill normal
         // 1: fill slower than the others
         // 0: don't fill at all
-
+    
+    public enum Status {WORKING, ONHOLD, NOTREADY}
+    private volatile Status status;
+    
+    private final AtomicInteger currentWorkload;
+    
     public StreamerCapacity(String hostname,
-            int hundredPercentCapacity,
-            int absoluteCapacity,
-            int priority) {
+                            int hundredPercentCapacity,
+                            int absoluteCapacity,
+                            int priority) {
         
         this.hostname = hostname;
+        this.hundredPercentCapacity = hundredPercentCapacity;
         this.absoluteCapacity = absoluteCapacity;
-        this.nominalCapacity = hundredPercentCapacity;
         this.priority = priority;
+        
+        this.status = Status.NOTREADY;
+        this.currentWorkload = new AtomicInteger(0);
     }
 
+    public void update(int hundredPercentCapacity,
+                       int absoluteCapacity,
+                       int priority) {
+        
+        this.hundredPercentCapacity = hundredPercentCapacity;
+        this.absoluteCapacity = absoluteCapacity;
+        this.priority = priority;
+    }
+    
     public String getHostname() {
         return hostname;
+    }
+
+    public int getHundredPercentCapacity() {
+        return hundredPercentCapacity;
     }
 
     public int getAbsoluteCapacity() {
         return absoluteCapacity;
     }
 
-    public int getNominalCapacity() {
-        return nominalCapacity;
-    }
-
     public int getPriority() {
         return priority;
     }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public int getCurrentWorkload() {
+        return currentWorkload.get();
+    }
+
+    public void incCurrentWorkload() {
+        currentWorkload.incrementAndGet();
+    }
     
+    public void decCurrentWorkload() {
+        int newValue = currentWorkload.decrementAndGet();
+        // There is a weakness here: Between these two lines a
+        // getCurrentWorkload() could report this value as being negative.
+        // But this is only an edge-case; the implementation with a
+        // synchronized object would be a bit over-engineered.
+        // Also: It should not get < 0 anyway!
+        if (newValue < 0) {
+            currentWorkload.set(newValue);
+        }
+    }
+    
+    @Override
+    public String toString() {
+        
+        return String.format(
+                "StreamerCapacity{hostname=%s, hundredPercentCapacity=%d, absoluteCapacity=%d, priority=%d, status=%s, currentWorkload=%d}",
+                hostname, hundredPercentCapacity, absoluteCapacity, priority, status, currentWorkload.get());
+    }
 }
