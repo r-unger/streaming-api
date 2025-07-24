aab1860edca7b12d00797ab519eeda91fa404036
fred42vid <Fred42vid@gmail.com>
2025-07-24 13:59:04 +0200

    replaced the deprecated 'new URL()'; plus some automatic formating changes

--- src/main/java/com/videotools/streamingapi/actors/HealthCheckActor.java
+++ src/main/java/com/videotools/streamingapi/actors/HealthCheckActor.java
@@ -17,6 +17,8 @@ import java.io.File;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.charset.Charset;
 import java.time.Duration;
@@ -43,6 +45,7 @@ public class HealthCheckActor extends AbstractActorWithTimers {
         public AreYouResponsive() {
         }
     }
+
     // Message to trigger the internal loop
     static public class HealthCheckLoop {
         public HealthCheckLoop() {
@@ -52,42 +55,41 @@ public class HealthCheckActor extends AbstractActorWithTimers {
 
     private final Object TIMERKEY;
     private final Duration LOOPWAIT;
-    
+
     private final LoggingAdapter logger;
     protected final ReceiveBuilder actorReceiveBlt;
-    
+
     private final SyncedStreamerInfo streamerInfo;
-    
+
     // health recordings
     private Instant sinceWhen;
     private List<Long> maxAge;
     private List<Boolean> onHold;
-        // both arrays are accessed with their index at
-        // streamerInfo.getCopyOfStreamerCapList()
-        // maxAge/onHold are NOT part of StreamerCapacity,
-        // because they are only for temporary statistics
-        // Hint: when the count & order of the StreamerCapList changes,
-        // then the order of these lists are also off
-    
+    // both arrays are accessed with their index at
+    // streamerInfo.getCopyOfStreamerCapList()
+    // maxAge/onHold are NOT part of StreamerCapacity,
+    // because they are only for temporary statistics
+    // Hint: when the count & order of the StreamerCapList changes,
+    // then the order of these lists are also off
+
     protected HealthCheckActor() {
-        
+
         logger = Logging.getLogger(getContext().getSystem(), this);
-        
+
         TIMERKEY = getSelf().path().name() + " Loop";
         LOOPWAIT = Duration.of(10, ChronoUnit.SECONDS);
-            // since several timeouts will let the loop duration be beyond
-            // 10 seconds, the messages may have to wait in the queue
-        
+        // since several timeouts will let the loop duration be beyond
+        // 10 seconds, the messages may have to wait in the queue
+
         streamerInfo = ActorSingleton.getInstance()
                 .getSyncedStreamerInfo();
-        
+
         // init health recordings
         resetHealthRecordings(5);
-                // to start with, it's 5 streamers;
-                // if more are needed, they're added
-        
-        actorReceiveBlt =
-                receiveBuilder()
+        // to start with, it's 5 streamers;
+        // if more are needed, they're added
+
+        actorReceiveBlt = receiveBuilder()
                 .match(AreYouResponsive.class, areYouResponsive -> {
                     logger.debug("Got request 'Are you responsive?'");
                 })
@@ -106,34 +108,33 @@ public class HealthCheckActor extends AbstractActorWithTimers {
             onHold.add(false);
         }
     }
-    
+
     private boolean liveStreamHealthy(String urlStr, int i) {
-        
+
         boolean healthy = false;
         long lastModSince = 0;
-        
+
         try {
-            URL url = new URL(urlStr);
+            URL url = new URI(urlStr).toURL();
             // can throw a MalformedURLException
             // (i.e. the inputName possibly has a typo)
-            HttpURLConnection httpCon
-                    = (HttpURLConnection) url.openConnection();
+            HttpURLConnection httpCon = (HttpURLConnection) url.openConnection();
             httpCon.setConnectTimeout(5000); // since it's internal, that must be enough
-            httpCon.setReadTimeout(5000);    // don't think that I need it
+            httpCon.setReadTimeout(5000); // don't think that I need it
             // can throw an IOException
             // (i.e. probably a network error)
             int httpCode = httpCon.getResponseCode();
-            //System.out.print("HTTP Code: " + httpCode);
+            // System.out.print("HTTP Code: " + httpCode);
             if ((httpCode == 200) || (httpCode == 206) || (httpCode == 304)) {
                 // resource is okay, so proceed
                 long lastModified = httpCon.getHeaderFieldDate("Last-Modified", 0);
                 lastModSince = Instant.now().toEpochMilli() - lastModified;
                 if (lastModSince < 0) {
                     logger.error("lastModified is in the future");
-                } else if (lastModSince > 24*60*60*1000) {
+                } else if (lastModSince > 24 * 60 * 60 * 1000) {
                     logger.error("lastModified is in more than a day old");
                 }
-                
+
                 if (lastModSince < 15000) { // < 15 seconds
                     healthy = true;
                 } else {
@@ -146,7 +147,7 @@ public class HealthCheckActor extends AbstractActorWithTimers {
                 logger.debug("URL '" + url.toExternalForm()
                         + "' connection with HTTP error code " + httpCode);
             }
-        } catch (MalformedURLException ex) {
+        } catch (URISyntaxException | MalformedURLException ex) {
             logger.error("URL {} is not correct; {}",
                     urlStr, ex.getMessage());
         } catch (IOException ex) {
@@ -155,12 +156,12 @@ public class HealthCheckActor extends AbstractActorWithTimers {
                     urlStr, ex.getMessage());
             // no need to log that error (only for debugging)
         }
-        
+
         if (!healthy) {
             // resize lists, if too short
-            if (maxAge.size() < i+1) {
+            if (maxAge.size() < i + 1) {
                 int oldSize = maxAge.size();
-                for (int j = oldSize; j < i+1; j++) {
+                for (int j = oldSize; j < i + 1; j++) {
                     maxAge.add(0L);
                     onHold.add(false);
                 }
@@ -177,22 +178,22 @@ public class HealthCheckActor extends AbstractActorWithTimers {
             }
             onHold.set(i, true);
         }
-        
+
         return healthy;
     }
-    
+
     // TODO: if stream not available at all, set StreamerCapacity.Status.NOTREADY
     private void doTheLoop() {
-        
+
         // reload streamer capacities from csv file
         streamerInfo.updateStreamersFromFile();
-        
+
         // for all found streamers ...
         boolean allHealthy = true;
         Object[] streamers = streamerInfo.getCopyOfStreamerCapList();
         // classical for-loop, because I need the index i
         for (int i = 0; i < streamers.length; i++) {
-            StreamerCapacity cap = (StreamerCapacity)(streamers[i]);
+            StreamerCapacity cap = (StreamerCapacity) (streamers[i]);
             String protocol = "https";
             String resource = "/play/livestream.m3u8";
             String urlStr = protocol + "://" + cap.getHostname() + resource;
@@ -203,7 +204,7 @@ public class HealthCheckActor extends AbstractActorWithTimers {
                 cap.setStatus(StreamerCapacity.Status.WORKING);
             }
         }
-        
+
         if ((allHealthy) && (sinceWhen != Instant.MAX)) {
             // this means that they're healthy now,
             // but some recordings were gathered
@@ -211,7 +212,7 @@ public class HealthCheckActor extends AbstractActorWithTimers {
             StringBuilder statsLine = new StringBuilder();
             statsLine.append(sinceWhen.toString()).append(", ");
             // onHold isn't needed, since healthy streamers have maxAge 0
-            maxAge.forEach((age) -> statsLine.append(age/1000.0).append(", "));
+            maxAge.forEach((age) -> statsLine.append(age / 1000.0).append(", "));
             try {
                 File statsFile = new File(statsFilename);
                 FileUtils.writeStringToFile(statsFile,
@@ -222,12 +223,12 @@ public class HealthCheckActor extends AbstractActorWithTimers {
                 logger.error(ex.getMessage());
                 logger.error("Could not write stats to {}", statsFilename);
             }
-            
+
             // reset everything (esp. sinceWhen = Instant.MAX)
             resetHealthRecordings(streamers.length);
         }
     }
-    
+
     @Override
     public void preStart() {
         logger.debug("preStart()");
@@ -244,5 +245,5 @@ public class HealthCheckActor extends AbstractActorWithTimers {
     public AbstractActor.Receive createReceive() {
         return actorReceiveBlt.build();
     }
-    
+
 }
--- src/main/java/com/videotools/streamingapi/util/StreamGetter.java
+++ src/main/java/com/videotools/streamingapi/util/StreamGetter.java
@@ -15,6 +15,8 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.time.Duration;
 import java.util.ArrayList;
@@ -27,7 +29,7 @@ import org.slf4j.LoggerFactory;
  * @author xyz
  */
 public class StreamGetter {
-    
+
     private final Logger logger;
     private final String m3u8Url;
     private final String baseUrl;
@@ -35,32 +37,31 @@ public class StreamGetter {
     private long firstSeqNo;
 
     public StreamGetter(String m3u8Url) {
-        
+
         logger = LoggerFactory.getLogger(getClass());
         this.m3u8Url = m3u8Url;
-        baseUrl = m3u8Url.substring(0, m3u8Url.lastIndexOf('/')+1);
+        baseUrl = m3u8Url.substring(0, m3u8Url.lastIndexOf('/') + 1);
         playlist = new ArrayList<>();
         firstSeqNo = -1;
     }
-    
+
     // TODO: for more reliable use outside this mini-project:
     // have this in the constructor, so that it's clear that it must be called first
     public void loadPlaylist()
-            throws MalformedURLException, IOException, FileNotFoundException {
-        
+            throws URISyntaxException, MalformedURLException, IOException, FileNotFoundException {
+
         playlist.clear();
-        URL url = new URL(m3u8Url);
-                // can throw a MalformedURLException
-                // (i.e. the inputName possibly has a typo)
+        URL url = new URI(m3u8Url).toURL();
+        // can throw a MalformedURLException
+        // (i.e. the inputName possibly has a typo)
         HttpURLConnection httpCon = (HttpURLConnection) url.openConnection();
-                // can throw an IOException
-                // (i.e. probably a network error)
+        // can throw an IOException
+        // (i.e. probably a network error)
         int httpCode = httpCon.getResponseCode();
         if ((httpCode == 200) || (httpCode == 206) || (httpCode == 304)) {
             // resource is okay, so proceed
-            try (BufferedReader reader =
-                   new BufferedReader(new InputStreamReader(httpCon.getInputStream()))) {
-                    // comment: BufferedReader isn't necessary, since it's in-memory copying
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(httpCon.getInputStream()))) {
+                // comment: BufferedReader isn't necessary, since it's in-memory copying
                 String line = null;
                 while ((line = reader.readLine()) != null) {
                     playlist.add(line);
@@ -73,33 +74,32 @@ public class StreamGetter {
                     + "' connection with HTTP error code " + httpCode);
         }
     }
-    
-    public byte[] loadStreamData(String streamUrl)
-            throws MalformedURLException, IOException, FileNotFoundException {
-        
-        ByteArrayOutputStream outputStream =
-                new ByteArrayOutputStream(8*1024*1024); // 8 MiB should be enough
 
-        URL url = new URL(streamUrl);
-                // can throw a MalformedURLException
-                // (i.e. the inputName possibly has a typo)
+    public byte[] loadStreamData(String streamUrl)
+            throws URISyntaxException, MalformedURLException, IOException, FileNotFoundException {
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream(8 * 1024 * 1024); // 8 MiB should be enough
+
+        URL url = new URI(streamUrl).toURL();
+        // can throw a MalformedURLException
+        // (i.e. the inputName possibly has a typo)
         HttpURLConnection httpConn = (HttpURLConnection) url.openConnection();
-                // can throw an IOException
-                // (i.e. probably a network error)
+        // can throw an IOException
+        // (i.e. probably a network error)
         int httpCode = httpConn.getResponseCode();
         if (httpCode == HttpURLConnection.HTTP_OK) {
-            // logger.debug("Content type:   {}", httpConn.getContentType());
+            // logger.debug("Content type: {}", httpConn.getContentType());
             // logger.debug("Content length: {}", httpConn.getContentLength());
-            
+
             // resource is okay, so proceed
             InputStream inputStream = httpConn.getInputStream();
- 
+
             int bytesRead = -1;
             byte[] buffer = new byte[BUFFER_SIZE];
             while ((bytesRead = inputStream.read(buffer)) != -1) {
                 outputStream.write(buffer, 0, bytesRead);
             }
- 
+
             outputStream.close();
             inputStream.close();
         } else {
@@ -108,41 +108,42 @@ public class StreamGetter {
                     + "' connection with HTTP error code " + httpCode);
             // but code 206 Partial Content also isn't accepted
         }
-        
+
         return outputStream.toByteArray();
     }
-    
+
     public long getFirstSequenceNo() {
-        
+
         firstSeqNo = -1; // returning -1 means error (or: no entry found)
-        
+
         ListIterator<String> playlistIterator = playlist.listIterator();
         while ((playlistIterator.hasNext()) && (firstSeqNo == -1)) {
             String line = playlistIterator.next();
             if (line.startsWith("#EXT-X-MEDIA-SEQUENCE")) {
-                String numberAsString = line.substring(line.lastIndexOf(':')+1);
+                String numberAsString = line.substring(line.lastIndexOf(':') + 1);
                 firstSeqNo = Long.parseUnsignedLong(numberAsString);
-//                System.out.println("Retrieved string '" + numberAsString + "', as number: " + firstSeqNo);
+                // System.out.println("Retrieved string '" + numberAsString + "', as number: " +
+                // firstSeqNo);
             }
         }
-        
+
         return firstSeqNo;
     }
-    
+
     public Duration getDurationOfSequenceNo(long seqNo) {
-        
+
         if (firstSeqNo == -1) {
             // should not happen, since getFirstSequenceNo()
             // should have been called already
             // but never-the-less:
             getFirstSequenceNo(); // to have it set
         }
-        
+
         Duration duration = Duration.ZERO;
-        
+
         // scan for the right url
         if (firstSeqNo != -1) {
-            long currentSeqNo = firstSeqNo-1;
+            long currentSeqNo = firstSeqNo - 1;
             ListIterator<String> playlistIterator = playlist.listIterator();
             while ((playlistIterator.hasNext()) && (duration.isZero())) {
                 String line = playlistIterator.next();
@@ -152,35 +153,35 @@ public class StreamGetter {
                         // expected format:
                         // #EXTINF:6.000,
                         String durAsString = line
-                                .substring(line.lastIndexOf(':')+1, line.length()-1);
+                                .substring(line.lastIndexOf(':') + 1, line.length() - 1);
                         duration = Duration
-                                .ofMillis((long)
-                                    (Double.parseDouble(durAsString)*1000.0));
-//                        logger.debug("#EXTINF line is '{}', extracted time info is '{}', as double that's {}, the duration obj. is {}.",
-//                                line, durAsString, Double.parseDouble(durAsString), duration);
+                                .ofMillis((long) (Double.parseDouble(durAsString) * 1000.0));
+                        // logger.debug("#EXTINF line is '{}', extracted time info is '{}', as double
+                        // that's {}, the duration obj. is {}.",
+                        // line, durAsString, Double.parseDouble(durAsString), duration);
                     }
                 }
             }
         }
-        
+
         return duration;
     }
-    
+
     public byte[] loadSequenceNo(long seqNo) {
-        
+
         if (firstSeqNo == -1) {
             // should not happen, since getFirstSequenceNo()
             // should have been called already
             // but never-the-less:
             getFirstSequenceNo(); // to have it set
         }
-        
+
         byte[] streamData = null;
-        
+
         // scan for the right url
         if (firstSeqNo != -1) {
             String streamUrl = null;
-            long currentSeqNo = firstSeqNo-1;
+            long currentSeqNo = firstSeqNo - 1;
             ListIterator<String> playlistIterator = playlist.listIterator();
             while ((playlistIterator.hasNext()) && (streamData == null)) {
                 String line = playlistIterator.next();
@@ -190,8 +191,8 @@ public class StreamGetter {
                         if (playlistIterator.hasNext()) {
                             line = playlistIterator.next();
                             streamUrl = baseUrl + line;
-//                            System.out.println("Found URL '" + streamUrl
-//                                    + "' for seqNo. " + seqNo);
+                            // System.out.println("Found URL '" + streamUrl
+                            // + "' for seqNo. " + seqNo);
                             try {
                                 streamData = loadStreamData(streamUrl);
                             } catch (Exception ex) {
@@ -209,33 +210,36 @@ public class StreamGetter {
                 logger.error("Tried to get sequence no. {}, but didn't find it in the playlist. Am I too late?", seqNo);
             }
         }
-        
+
         return streamData;
     }
-    
+
     // example method from
     // https://www.codejava.net/java-se/networking/use-httpurlconnection-to-download-file-from-an-http-url
-    // call: downloadFile(streamUrl, "/Users/xyz/projects/video-tools/streaming-loadtest");
+    // call: downloadFile(streamUrl,
+    // "/Users/xyz/projects/video-tools/streaming-loadtest");
     private static final int BUFFER_SIZE = 4096;
-     /**
+
+    /**
      * Downloads a file from a URL
+     * 
      * @param fileURL HTTP URL of the file to be downloaded
      * @param saveDir path of the directory to save the file
      * @throws IOException
      */
     public static void downloadFile(String fileURL, String saveDir)
-            throws IOException {
-        URL url = new URL(fileURL);
+            throws URISyntaxException, IOException {
+        URL url = new URI(fileURL).toURL();
         HttpURLConnection httpConn = (HttpURLConnection) url.openConnection();
         int responseCode = httpConn.getResponseCode();
- 
+
         // always check HTTP response code first
         if (responseCode == HttpURLConnection.HTTP_OK) {
             String fileName = "";
             String disposition = httpConn.getHeaderField("Content-Disposition");
             String contentType = httpConn.getContentType();
             int contentLength = httpConn.getContentLength();
- 
+
             if (disposition != null) {
                 // extracts file name from header field
                 int index = disposition.indexOf("filename=");
@@ -248,33 +252,33 @@ public class StreamGetter {
                 fileName = fileURL.substring(fileURL.lastIndexOf("/") + 1,
                         fileURL.length());
             }
- 
+
             System.out.println("Content-Type = " + contentType);
             System.out.println("Content-Disposition = " + disposition);
             System.out.println("Content-Length = " + contentLength);
             System.out.println("fileName = " + fileName);
- 
+
             // opens input stream from the HTTP connection
             InputStream inputStream = httpConn.getInputStream();
             String saveFilePath = saveDir + File.separator + fileName;
-             
+
             // opens an output stream to save into file
             FileOutputStream outputStream = new FileOutputStream(saveFilePath);
- 
+
             int bytesRead = -1;
             byte[] buffer = new byte[BUFFER_SIZE];
             while ((bytesRead = inputStream.read(buffer)) != -1) {
                 outputStream.write(buffer, 0, bytesRead);
             }
- 
+
             outputStream.close();
             inputStream.close();
- 
+
             System.out.println("File downloaded");
         } else {
             System.out.println("No file to download. Server replied HTTP code: " + responseCode);
         }
         httpConn.disconnect();
     }
-    
+
 }
